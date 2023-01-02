6118f04f86156d1c97b31da09f92235154398b59
Emily LaBelle <emilylabelle8@gmail.com>
2023-01-02 10:50:26 -0500

    added drop down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,12 +14,54 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Stack from '@mui/material/Stack';
 
 
 
 const drawerWidth = 240;
 
 export default function DrawerAppBar(props) {
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   const navigate = useNavigate();
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -68,6 +110,10 @@ export default function DrawerAppBar(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
+<>
+     
+
+
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar component="nav">
@@ -94,9 +140,56 @@ export default function DrawerAppBar(props) {
             </Button>
             <Button sx={{ color: '#fff' }} onClick={navigateDiscounted}>
               Deals
-            </Button>
-            <Button sx={{ color: '#fff' }} onClick={navigateItems}>
-              Items
+            </Button> 
+            <Button sx={{ color: '#fff' }} onClick={navigateItems}> item
+            <Stack direction="row" spacing={2}>
+        <div>
+          <Button 
+            ref={anchorRef}
+            id="composition-button"
+            aria-controls={open ? 'composition-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+          >
+            Dashboard
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === 'bottom-start' ? 'left top' : 'left bottom',
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-button"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <MenuItem onClick={handleClose}>Laptops</MenuItem>
+                      <MenuItem onClick={handleClose}>Smart Phones</MenuItem>
+                      <MenuItem onClick={handleClose}>Skin Care</MenuItem>
+                      <MenuItem onClick={handleClose}>Fragrances</MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      </Stack>
             </Button>
           </Box>
         </Toolbar>
@@ -122,5 +215,9 @@ export default function DrawerAppBar(props) {
         <Toolbar />
       </Box>
     </Box>
+    </>
   );
 }
+
+
+
