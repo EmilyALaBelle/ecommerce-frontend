3125b8253a5d57eaee72dfc1c411473bf5adce0f
Emily LaBelle <emilylabelle8@gmail.com>
2023-01-02 11:08:22 -0500

    added all products to naviagtion bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,17 +81,17 @@ export default function DrawerAppBar(props) {
     navigate('/laptops')
   }
 
-const navigateSkincare = () => {
-  navigate('/skincare')
-}
+  const navigateSkincare = () => {
+    navigate('/skincare')
+  }
 
-const navigateSmartphones = () => {
-  navigate('/smartphones')
-}
+  const navigateSmartphones = () => {
+    navigate('/smartphones')
+  }
 
-const navigateFragrances = () => {
-  navigate('/fragrances')
-}
+  const navigateFragrances = () => {
+    navigate('/fragrances')
+  }
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
@@ -126,111 +126,112 @@ const navigateFragrances = () => {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-<>
-     
+    <>
 
 
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <Button sx={{ color: '#fff' }} onClick={navigateHome}>
-              Home
-            </Button>
-            <Button sx={{ color: '#fff' }} onClick={navigateDiscounted}>
-              Deals
-            </Button> 
-            <Button sx={{ color: '#fff' }} > item
-            <Stack direction="row" spacing={2}>
-        <div>
-          <Button 
-            ref={anchorRef}
-            id="composition-button"
-            aria-controls={open ? 'composition-menu' : undefined}
-            aria-expanded={open ? 'true' : undefined}
-            aria-haspopup="true"
-            onClick={handleToggle}
-          >
-            Dashboard
-          </Button>
-          <Popper
-            open={open}
-            anchorEl={anchorRef.current}
-            role={undefined}
-            placement="bottom-start"
-            transition
-            disablePortal
-          >
-            {({ TransitionProps, placement }) => (
-              <Grow
-                {...TransitionProps}
-                style={{
-                  transformOrigin:
-                    placement === 'bottom-start' ? 'left top' : 'left bottom',
-                }}
-              >
-                <Paper>
-                  <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList
-                      autoFocusItem={open}
-                      id="composition-menu"
-                      aria-labelledby="composition-button"
-                      onKeyDown={handleListKeyDown}
+
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              MUI
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <Button sx={{ color: '#fff' }} onClick={navigateHome}>
+                Home
+              </Button>
+              <Button sx={{ color: '#fff' }} onClick={navigateDiscounted}>
+                Deals
+              </Button>
+              <Button sx={{ color: '#fff' }} > item
+                <Stack direction="row" spacing={2}>
+                  <div>
+                    <Button
+                      ref={anchorRef}
+                      id="composition-button"
+                      aria-controls={open ? 'composition-menu' : undefined}
+                      aria-expanded={open ? 'true' : undefined}
+                      aria-haspopup="true"
+                      onClick={handleToggle}
                     >
-                      <MenuItem onClick={navigateLaptops}>Laptops</MenuItem>
-                      <MenuItem onClick={navigateSmartphones}>Smart Phones</MenuItem>
-                      <MenuItem onClick={navigateSkincare}>Skin Care</MenuItem>
-                      <MenuItem onClick={navigateFragrances}>Fragrances</MenuItem>
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
-              </Grow>
-            )}
-          </Popper>
-        </div>
-      </Stack>
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
+                      Dashboard
+                    </Button>
+                    <Popper
+                      open={open}
+                      anchorEl={anchorRef.current}
+                      role={undefined}
+                      placement="bottom-start"
+                      transition
+                      disablePortal
+                    >
+                      {({ TransitionProps, placement }) => (
+                        <Grow
+                          {...TransitionProps}
+                          style={{
+                            transformOrigin:
+                              placement === 'bottom-start' ? 'left top' : 'left bottom',
+                          }}
+                        >
+                          <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                              <MenuList
+                                autoFocusItem={open}
+                                id="composition-menu"
+                                aria-labelledby="composition-button"
+                                onKeyDown={handleListKeyDown}
+                              >
+                                <MenuItem onClick={navigateLaptops}>Laptops</MenuItem>
+                                <MenuItem onClick={navigateSmartphones}>Smart Phones</MenuItem>
+                                <MenuItem onClick={navigateSkincare}>Skin Care</MenuItem>
+                                <MenuItem onClick={navigateFragrances}>Fragrances</MenuItem>
+                                <MenuItem onClick={navigateItems}>All Products</MenuItem>
+                              </MenuList>
+                            </ClickAwayListener>
+                          </Paper>
+                        </Grow>
+                      )}
+                    </Popper>
+                  </div>
+                </Stack>
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box component="main" sx={{ p: 3 }}>
+          <Toolbar />
+        </Box>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-      </Box>
-    </Box>
     </>
   );
 }
